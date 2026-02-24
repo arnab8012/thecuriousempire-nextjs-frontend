ef17708d9317e5972b34a9f4e243014bef9c93f5
Arnab Chowdhury <arnabchowdhury801@gmail.com>
2026-02-24 23:35:14 +0600

    Update useNavigate.ts

--- src/utils/useNavigate.ts
+++ src/utils/useNavigate.ts
@@ -2,12 +2,10 @@
 
 import { useRouter } from "next/navigation";
 
-// React Router compatible navigate()
 export function useNavigate() {
   const router = useRouter();
-  return (to, options = {}) => {
-    const href = typeof to === "string" ? to : String(to);
-    if (options?.replace) router.replace(href);
-    else router.push(href);
+
+  return (to: string) => {
+    router.push(to);
   };
 }
