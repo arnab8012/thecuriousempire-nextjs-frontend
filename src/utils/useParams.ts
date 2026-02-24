4f4257304733730d640e6500c158282da62880cf
Arnab Chowdhury <arnabchowdhury801@gmail.com>
2026-02-24 23:35:53 +0600

    Update useParams.ts

--- src/utils/useParams.ts
+++ src/utils/useParams.ts
@@ -2,13 +2,6 @@
 
 import { useParams as useNextParams } from "next/navigation";
 
-export function useParams() {
-  const p = useNextParams() || {};
-  // next returns string | string[]; keep string
-  const out = {};
-  for (const k of Object.keys(p)) {
-    const v = p[k];
-    out[k] = Array.isArray(v) ? v[0] : v;
-  }
-  return out;
+export function useParams<T extends Record<string, string>>() {
+  return useNextParams() as unknown as T;
 }
