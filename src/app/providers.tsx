08d2586e0a942d110d69479c883426307194106a
Arnab Chowdhury <arnabchowdhury801@gmail.com>
2026-02-27 21:43:52 +0600

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,20 @@
 "use client";
 
+import React from "react";
+import { HelmetProvider } from "react-helmet-async";
+
 import { AuthProvider } from "@/context/AuthContext";
 import { CartProvider } from "@/context/CartContext";
 import { FavoritesProvider } from "@/context/FavoritesContext";
 
-export default function Providers({ children }) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <AuthProvider>
+    <HelmetProvider>
       <CartProvider>
         <FavoritesProvider>
-          {children}
+          <AuthProvider>{children}</AuthProvider>
         </FavoritesProvider>
       </CartProvider>
-    </AuthProvider>
+    </HelmetProvider>
   );
 }
