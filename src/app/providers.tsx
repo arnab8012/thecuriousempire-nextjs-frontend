9c38e75cf04ab8f72767756ea7e4f0f214003d5e
Arnab Chowdhury <arnabchowdhury801@gmail.com>
2026-02-27 17:50:31 +0600

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import React from "react";
 import { AuthProvider } from "@/context/AuthContext";
 import { CartProvider } from "@/context/CartContext";
 import { FavoritesProvider } from "@/context/FavoritesContext";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export default function Providers({ children }) {
   return (
     <AuthProvider>
       <CartProvider>
-        <FavoritesProvider>{children}</FavoritesProvider>
+        <FavoritesProvider>
+          {children}
+        </FavoritesProvider>
       </CartProvider>
     </AuthProvider>
   );
