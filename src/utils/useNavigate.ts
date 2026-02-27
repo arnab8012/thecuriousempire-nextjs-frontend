4cce7853ff2609c79688fcb11f1ce2247977d5b4
Arnab Chowdhury <arnabchowdhury801@gmail.com>
2026-02-27 15:43:24 +0600

    Update useNavigate.ts

--- src/utils/useNavigate.ts
+++ src/utils/useNavigate.ts
@@ -1,11 +1,10 @@
-"use client";
-
 import { useRouter } from "next/navigation";
 
 export function useNavigate() {
   const router = useRouter();
 
-  return (to: string) => {
-    router.push(to);
+  return (to: string, options?: { replace?: boolean }) => {
+    if (options?.replace) router.replace(to);
+    else router.push(to);
   };
 }
