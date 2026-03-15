69913391c10e67dda66758af76a01a285cb6df56
Arnab Chowdhury <arnabchowdhury801@gmail.com>
2026-03-15 23:22:13 +0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,7 +72,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1591140458767669');
+              fbq('init', '1572683880502321');
               fbq('track', 'PageView');
             `,
           }}
@@ -86,7 +86,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1591140458767669&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1572683880502321&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
