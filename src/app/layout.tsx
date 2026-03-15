5b221eb9efeb9b09b29b3b0b887f46aa9e9b2f58
Arnab Chowdhury <arnabchowdhury801@gmail.com>
2026-03-15 20:22:04 +0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export const metadata: Metadata = {
   },
 };
 
-const META_PIXEL_ID = "1591140458767669";
 const GA_ID = "G-PBDK1ZQPKL";
 
 export default function RootLayout({
@@ -44,6 +43,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(orgLd) }}
         />
 
+        {/* Google Analytics */}
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
           strategy="afterInteractive"
@@ -58,6 +58,7 @@ export default function RootLayout({
           `}
         </Script>
 
+        {/* Meta Pixel Code */}
         <Script
           id="meta-pixel-base"
           strategy="afterInteractive"
@@ -66,26 +67,26 @@ export default function RootLayout({
               !function(f,b,e,v,n,t,s)
               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;
-              n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];
-              t=b.createElement(e);t.async=!0;
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
               t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}
-              (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${META_PIXEL_ID}');
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1591140458767669');
               fbq('track', 'PageView');
             `,
           }}
         />
       </head>
+
       <body>
+        {/* Meta Pixel noscript */}
         <noscript>
           <img
             height="1"
             width="1"
             style={{ display: "none" }}
-            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            src="https://www.facebook.com/tr?id=1591140458767669&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
